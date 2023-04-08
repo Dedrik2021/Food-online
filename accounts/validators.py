cfabb5de5684dd0dc7be9d48b6256cfdeb7269a9
Oleg <plumbumbox4@gmail.com>
2023-04-08 16:24:29 +0200

    vendor profile from + validation

---
+++ accounts/validators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+import os
+
+
+def allow_only_images_validator(value):
+    ext = os.path.splitext(value.name)[1]
+    print(ext)
+    valid_extension = ['.csv']
+
+    if not ext.lower() in valid_extension:
+        raise ValidationError(f"Unsupported file extension. Allowed extension: {valid_extension}") 
